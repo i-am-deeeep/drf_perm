51153eb265d99f5c4056c326ac21910fa71c74db
Subhodeep Kumar Singh <subhodeep2791999@gmail.com>
2024-10-18 03:30:23 +0530

    started permission

--- drf_perm/drf_app/views.py
+++ drf_perm/drf_app/views.py
@@ -3,6 +3,7 @@ from .models import Movie, Review
 from drf_app.serializers import MovieSerializer, ReviewSerializer
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class MovieListAV(APIView):
@@ -50,6 +51,7 @@ class MovieDetailAV(APIView):
 
 
 class ReviewListAV(APIView):
+    permission_classes=[IsAuthenticated]
     def get(self,request):
         # print(request.user)
         reviews=Review.objects.all()
